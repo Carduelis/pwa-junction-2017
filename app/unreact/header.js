18ee86418c7faccd1c6c46559adc4bfb51c982ba
Pavel <pavepy@yandex.ru>
2017-11-26 08:33:38 +0300

    new icons

--- app/unreact/header.js
+++ app/unreact/header.js
@@ -1,6 +1,10 @@
 import $ from 'jquery';
-export default $(`
+const $header = $(`
 		<h1 class="results-title">
 			Concert Fly <span>Trips</span>
 		</h1>
 `);
+$header.on('click', function() {
+	window.location.href = '/';
+});
+export default $header;
